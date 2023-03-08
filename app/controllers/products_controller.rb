99ff4a8aeb61188d9ae3e60e139492a68435a8b9
Stephen Larsen <slarsendev@gmail.com>
2023-03-08 12:20:42 -0800

    ProdcutsController: Changed calculate action logic

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -39,11 +39,20 @@ class ProductsController < ApplicationController
   end
 
   def calculate
-    @product = Product.find_by(calculate_params(params))
-    if @product
-      render json: { product_name: "#{@product.type} - #{@product.name.split.first}" }, status: :ok
+    @filtered_params = calculate_params(params)
+    
+    if !is_value_zero?
+      @product = Product.where(:length.gt => @filtered_params[:length],
+                              :height.gt => @filtered_params[:height],
+                              :width.gte => @filtered_params[:width],
+                              :weight.gte => @filtered_params[:weight]).order_by(length: :asc).first
+      if @product
+        render json: { product_name: "#{@product.type} - #{@product.name}" }, status: :ok
+      else
+        render json: { message: 'Product not found' }, status: :not_found
+      end
     else
-      render json: { message: 'Product not found' }, status: :not_found
+      render json: { message: 'Please provide the right input' }, status: :not_found
     end
   end
 
@@ -54,7 +63,7 @@ class ProductsController < ApplicationController
 
     def calculate_params(params)
       params = params.permit(:name, :length, :width, :height, :weight).to_h
-      Hash[params.map { |k,v| [k, v.to_i ]}]
+      Hash[params.map { |k,v| [k.to_sym, v.to_i ]}]
     end
 
     def find_product
@@ -64,6 +73,10 @@ class ProductsController < ApplicationController
 
     def product
       @product = Product.find_by(params[:id])
-      render json: { message: 'Product not found.' }, status: :unprocessable_entity and return if @product.blank?
+      render json: { message: 'Product not found.' }, status: :not_found and return if @product.blank?
+    end
+
+    def is_value_zero?
+      @filtered_params[:length] == 0 || @filtered_params[:height] == 0 || @filtered_params[:width] == 0 || @filtered_params[:weight] == 0
     end
 end
